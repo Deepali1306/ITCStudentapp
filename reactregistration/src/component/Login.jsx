6806138caa9be459303f06354501b7093b9ae822
Deepali1306 <deepalisingh9996@gmail.com>
2025-04-28 12:04:13 +0530

    Update Login.jsx

--- reactregistration/src/component/Login.jsx
+++ reactregistration/src/component/Login.jsx
@@ -1,26 +1,43 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 function Login() {
+  async function senddata(e){
+    e.preventDefault();
+    
+    const email=e.target.email.value;
+    const password=e.target.password.value;
+    // alert(name+email+password);
+   const response= await  fetch("https://itcstudentapp-backend-3s82.onrender.com",{
+      method:'POST',
+      body:JSON.stringify({email,password}),
+      headers:{'content-Type':'application/json'}
+      })
+    const res=await response.json();
+    alert(res.msg);
+
+  }
+
+  
   return (
-    <div>Login
+    <div>
         <div>
-        <form>
+        <form onSubmit={senddata}>
   <div class="form-group">
+ 
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
   </div>
   <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary">Login</button>
-</form>
+  <button type="submit" class="btn btn-primary">LOGIN</button>
+</form>  
         </div>
     </div>
   )
